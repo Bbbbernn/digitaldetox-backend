9676746a40f186b73d5e0244eff7ea6f765387cf
Bbbbernn <gabriele.bernassola26@gmail.com>
2026-03-28 00:43:10 +0100

    aggiornato a time zone Europe Rome

--- src/main/java/com/digitaldetox/DigitalDetoxApplication.java
+++ src/main/java/com/digitaldetox/DigitalDetoxApplication.java
@@ -1,14 +1,23 @@
 package com.digitaldetox;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 public class DigitalDetoxApplication {
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Rome"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DigitalDetoxApplication.class, args);
     }
